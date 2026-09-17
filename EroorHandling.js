519e5c28314f50d202f1365e147f4628bb7c30e0
Milinm1708 <mehtamilin74@gmail.com>
2026-09-17 10:14:11 +0530

    learn simple error handling concept

---
+++ EroorHandling.js
@@ -0,0 +1,17 @@
+//Eroor Handling
+
+// try {
+//     riskyOperation();
+// } catch (error) {
+//     console.log(error);
+// }
+
+
+
+
+//throw
+
+// if (!user) {
+//     throw new Error("User not found");
+// }
+
