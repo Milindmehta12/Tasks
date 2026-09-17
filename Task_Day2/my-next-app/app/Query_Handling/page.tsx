9c4216b0aaa5d16eda1e8617358ab2ed2dd1ecad
Milinm1708 <mehtamilin74@gmail.com>
2026-09-17 14:35:19 +0530

    Add Day 2 tasks

---
+++ Task_Day2/my-next-app/app/Query_Handling/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+export default function Products() {
+  const searchParams = useSearchParams();
+
+  const category = searchParams.get("category");
+  const price = searchParams.get("price");
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <p>Category: {category}</p>
+      <p>Price: {price}</p>
+    </div>
+  );
+}
---
+++ Task_Day2/my-next-app/app/TailWindcss/page.tsx
@@ -0,0 +1,13 @@
+export default function Tailwind() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">
+        Hello Tailwind CSS
+      </h1>
+
+      <button className="mt-5 rounded bg-blue-500 px-4 py-2 text-white">
+        Click Me
+      </button>
+    </div>
+  );
+}
